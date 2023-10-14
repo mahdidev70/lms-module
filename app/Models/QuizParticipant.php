0a4cdbf3171a6c54678929339735f83280eb0206
AmirMahdi-for <amirmahdifor@gmail.com>
2023-10-14 09:51:57 +0330

    quiz practicipant table migration

---
+++ app/Models/QuizParticipant.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TechStudio\Lms\app\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuizParticipant extends Model
+{
+    use HasFactory;
+
+    protected $table = 'lms_quiz_participants';
+
+    protected $guarded = [];
+    
+    protected $casts = [
+        'selected_choices' => 'array',
+    ];
+}
---
+++ database/migrations/2023_08_16_085546_create_quiz_participants_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lms_quiz_participants', function (Blueprint $table) {
+            $table->id();
+            $table->integer('lesson_id');
+            $table->integer('user_id');
+            $table->json('selected_choices')->nullable();
+            $table->integer('score')->nullable();
+            $table->enum('status',['start', 'fail', 'success']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lms_quiz_participants');
+    }
+};
