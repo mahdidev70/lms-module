0cdb849704e4a063cd97998c72b7ac2420c157de
AmirMahdi-for <amirmahdifor@gmail.com>
2023-10-14 08:44:24 +0330

    student table migration

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -9,6 +9,8 @@ class Student extends Model
 {
     use HasFactory;
 
+    protected $table = 'lms_students';
+
     protected $guarded = ['id'];
 
     public function course()
---
+++ database/migrations/2023_08_15_085311_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lms_students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('course_id');
+            $table->enum('in_roll', ['progress', 'done'])->nullable();
+            $table->boolean('bookmark')->nullable();
+            $table->integer('rate')->nullable();
+            $table->string('certificate_file')->nullable();
+            $table->text('comment')->nullabel();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lms_students');
+    }
+};
