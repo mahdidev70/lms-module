d8595428d328f78585527c154e7db673594e90a6
AmirMahdi-for <amirmahdifor@gmail.com>
2023-11-21 14:42:27 +0330

    add certificate resource file in module

---
+++ app/Http/Resources/CertificateResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Lms;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CertificateResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'displayName' => $this->userProfile->getDisplayName(),
+            'course' => $this->course->title,
+            'category' => $this->course->category->title,
+            'finalScore' => 98,
+            'certificateFile' => 'https://storage.sa-test.techstudio.diginext.ir/static/amirmahdi.jpg',
+        ];
+    }
+}
