987fa7b879f718b0c2637e3d0e9db24ecbc1c938
AmirMahdi-for <amirmahdifor@gmail.com>
2023-11-22 17:14:48 +0330

    add new changes for chapter repository

--- app/Repositories/ChapterRepository.php
+++ app/Repositories/ChapterRepository.php
@@ -1,20 +1,34 @@
 <?php
 
-namespace App\Repositories;
+namespace TechStduio\Lms\app\Repositories;
 
-use App\Helper\SlugGenerator;
-use App\Models\Chapter;
-use App\Repositories\Interfaces\ChapterRepositoryInterface;
+use TechStudio\Core\app\Helper\SlugGenerator;
+use Illuminate\Support\Facades\Auth;
+use TechStudio\Lms\app\Models\Chapter;
+use TechStudio\Lms\app\Models\Student;
+use TechStudio\Lms\app\Repositories\Interfaces\ChapterRepositoryInterface;
 
+// use TechStduio\Lms\app\Repositories\Interface
 
 class ChapterRepository implements ChapterRepositoryInterface
 {
     public function getBySlug($slug)
     {
-        return $chapter = Chapter::where(
-            'slug',
-            $slug
-        )->with('course')->firstOrFail();
+        $chapter = Chapter::where('slug', $slug)->with('course')->firstOrFail();
+
+        $user = Auth::user();
+
+        $student = Student::updateOrCreate(
+            ['user_id' => $user->id],
+            [
+                'user_id' => $user->id,
+                'course_id' => $chapter->course_id,
+                'in_roll' => 'progress',
+            ]
+        );
+
+        $student->save();
+        return $chapter;
     }
 
     public function createUpdate($data)
