6e1aec81998a9139f14c77cb214b990fe0006699
AmirMahdi-for <amirmahdifor@gmail.com>
2023-11-25 11:12:30 +0330

    change namespace of migrations

--- database/migrations/2023_07_29_124351_create_lessons_table.php
+++ database/migrations/2023_07_29_124351_create_lessons_table.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace TechStudio\Lms\database\Migrations;
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,6 +21,7 @@ return new class extends Migration
             $table->integer('order')->nullable();
             $table->enum('dominant_type', ['text', 'video', 'exam']);
             $table->json('content');
+            $table->string('duration')->nullable();
             $table->json('information')->nullable();
             $table->timestamps();
         });
--- database/migrations/2023_07_29_124429_create_user_lesson_progress_table.php
+++ database/migrations/2023_07_29_124429_create_user_lesson_progress_table.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace TechStudio\Lms\database\Migrations;
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
