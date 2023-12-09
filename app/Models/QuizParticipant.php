74c9c0e6c0fed07531199d756f7b669ef1804330
Mahdi Samian <91634743+mahdidev70@users.noreply.github.com>
2023-12-09 12:23:26 +0330

    fix: test deployment

--- app/Models/QuizParticipant.php
+++ app/Models/QuizParticipant.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class QuizParticipant extends Model
 {
     use HasFactory;
-
     protected $table = 'lms_quiz_participants';
 
     protected $guarded = ['id'];
