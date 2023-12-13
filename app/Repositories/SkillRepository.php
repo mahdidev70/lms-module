be7be5717fadc5c085401e3d3024c05f85eb8257
Amirmahdi-for <amirmahdifor@gmail.com>
2023-12-13 18:04:00 +0330

    add status for common of skill

--- app/Repositories/SkillRepository.php
+++ app/Repositories/SkillRepository.php
@@ -53,12 +53,17 @@ class SkillRepository implements SkillRepositoryInterface
 
     public function getCommonSkill() 
     {
+        $counts = [
+            'all' => Skill::all()->count(),
+            'active' => Skill::where('status', 1)->count(),
+            'hidden' => Skill::where('status', 0)->count(),
+        ];
+
+        $status = ['active','hidden','deleted'];
+
         return [
-            'counts' => [
-                'all' => Skill::all()->count(),
-                'active' => Skill::where('status', 1)->count(),
-                'hidden' => Skill::where('status', 0)->count(),
-            ]
+            'counts' => $counts,
+            'status' => $status, 
         ];
     }
 }
