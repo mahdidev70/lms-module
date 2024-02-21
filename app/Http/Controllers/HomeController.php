38426a3d6304fbdc36f4b3df0007e6e0a7119467
behzadi <fateme.behzadi@diginext.ir>
2024-02-21 14:19:57 +0330

    AcademyHomepageData cache for Academy homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace TechStudio\Lms\app\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
 use stdClass;
 use App\Http\Controllers\Controller;
 use TechStudio\Lms\app\Http\Resources\HomePageResource;
@@ -23,14 +24,27 @@ class HomeController extends Controller
 
     public function index()
     {
-        $data = new stdClass();
+      /*  $data = new stdClass();
         $data->landingBannerUrl = 'first url';
         $data->categories = $this->categoryRepository->getCategoriesWithCourses();
         $data->outPutSqureUpBannerUrl = 'second url';
         $data->outPutSqureDownBannerUrl = 'third url';
         $data->outPutRectangleBannerUrl = 'fourth url';
-        $data->comments = $this->commentRepository->getStarComments();
-        return response()->json(new HomePageResource($data));
+        $data->comments = $this->commentRepository->getStarComments();*/
+        $keyName = 'CommunityHomepageData';
+        $minutes = config('cache.long_time')??10080;
+        $cachedData = Cache::remember($keyName, $minutes, function () {
+            $data = new stdClass();
+            $data->landingBannerUrl = 'first url';
+            $data->categories = $this->categoryRepository->getCategoriesWithCourses();
+            $data->outPutSqureUpBannerUrl = 'second url';
+            $data->outPutSqureDownBannerUrl = 'third url';
+            $data->outPutRectangleBannerUrl = 'fourth url';
+            $data->comments = $this->commentRepository->getStarComments();
+            return $data;
+        });
+
+        return response()->json(new HomePageResource($cachedData));
     }
 
 }
