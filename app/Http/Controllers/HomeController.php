4d1a2aac60cfd8ddf424cab49a8be8d973b20941
behzadi <fateme.behzadi@diginext.ir>
2024-02-21 14:21:40 +0330

    AcademyHomepageData cache for Academy homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,7 +31,7 @@ class HomeController extends Controller
         $data->outPutSqureDownBannerUrl = 'third url';
         $data->outPutRectangleBannerUrl = 'fourth url';
         $data->comments = $this->commentRepository->getStarComments();*/
-        $keyName = 'CommunityHomepageData';
+        $keyName = 'LmsHomepageData';
         $minutes = config('cache.long_time')??10080;
         $cachedData = Cache::remember($keyName, $minutes, function () {
             $data = new stdClass();
