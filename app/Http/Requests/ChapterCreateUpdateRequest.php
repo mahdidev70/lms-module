d25f21b3e93728fdc54034dbe449212b0ad74721
Mahdi <mahdidev70@gmail.com>
2024-03-06 12:35:02 +0330

    fix: ignore current id

--- app/Http/Requests/ChapterCreateUpdateRequest.php
+++ app/Http/Requests/ChapterCreateUpdateRequest.php
@@ -23,7 +23,7 @@ class ChapterCreateUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => ['string', 'required', 'unique:lms_lessons,'.$this->id],
+            'title' => ['string', 'required', Rule::unique('lms_lessons')->ignore($this->id)],
             'courseId' => 'required|integer',
             'description' => 'string',
             'order' => 'integer',
