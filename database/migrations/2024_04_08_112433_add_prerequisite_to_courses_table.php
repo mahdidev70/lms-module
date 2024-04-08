26c2414e92ae4cd3368948e7fc85b6b4cfa5b4e6
next <mahdidev70@gmail.com>
2024-04-08 12:19:47 +0330

    feat: add preequisite field

--- database/migrations/2024_04_08_112433_add_prerequisite_to_courses_table.php
+++ database/migrations/2024_04_08_112433_add_prerequisite_to_courses_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('courses', function (Blueprint $table) {
+        Schema::table('lms_courses', function (Blueprint $table) {
             $table->json('prerequisites')->nullable()->after('level');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('courses', function (Blueprint $table) {
+        Schema::table('lms_courses', function (Blueprint $table) {
             $table->dropColumn('prerequisites');
         });
     }
