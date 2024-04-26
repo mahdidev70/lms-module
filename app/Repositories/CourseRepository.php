701f04eef6c9d1981501b641053927bb83c72542
Mahdi <mahdidev70@gmail.com>
2024-04-26 19:44:59 +0330

    feat: update course when add new chapter

--- app/Repositories/CourseRepository.php
+++ app/Repositories/CourseRepository.php
@@ -221,6 +221,6 @@ class CourseRepository implements CourseRepositoryInterface
     
     public function updateCourseEditeTime($request)
     {
-        return  Course::where('id', $request)->update(['updated_an'=> Carbon::now()]);
+        return  Course::where('id', $request)->update(['updated_at'=> Carbon::now()]);
     }
 }
