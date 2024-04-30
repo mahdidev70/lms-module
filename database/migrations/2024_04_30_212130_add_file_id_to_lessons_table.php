b172beb8f363095aaef5eff9f29803581815da93
next <mahdidev70@gmail.com>
2024-04-30 21:24:10 +0330

    fix: add file id to lesson

---
+++ database/migrations/2024_04_30_212130_add_file_id_to_lessons_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('lms_lessons', function (Blueprint $table) {
+            $table->string('file_id')->nullable()->after('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('lms_lessons', function (Blueprint $table) {
+            $table->dropColumn('file_id');
+        });
+    }
+};
